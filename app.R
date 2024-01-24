483a16eafe6d674d85a29e6a2b208e79694770c8
Steve-A-Mallett <129425738+Steve-A-Mallett@users.noreply.github.com>
2024-01-24 16:09:12 +0000

    Updated 24-Jan-2024

--- app.R
+++ app.R
@@ -6,20 +6,20 @@ library(ggtext)
 library(emojifont)
 library(tidyr)
 library(reshape2)
-#
+
 ui <- fluidPage(
   
   tags$style("h1 { color: #0570b0;}"),
-  tags$style("h2 { color: #74a9cf;}"),
-  tags$style("h3 { color: black;}"),
+  tags$style("h2 { color: #0570b0;}"),
+  tags$style("h3 { color: #0570b0;}"),
   tags$style("h4 { color: black;}"),
   
   tabsetPanel(
     id = "tabset",
-    tabPanel(h1("Introduction"),
+    tabPanel(h2("Introduction"),
              fluidRow(
                column(6,
-               h2("Make a New Asthma Medicine"),
+               h3("Make a New Asthma Medicine"),
                h4("Have you ever wondered how statistics could be useful in the real world?"),
                h4("Imagine that scientists have produced a new type of asthma inhaler. 
                   Any new medicine first needs to be tested in a clinical trial to be sure it works and hasn't any serious side effects."),
@@ -29,7 +29,7 @@ ui <- fluidPage(
                column(3,
                       imageOutput("photo"))
              )),
-    tabPanel(h1("Let's Go!"),
+    tabPanel(h2("Let's Go!"),
              fluidRow(
                column(3,
                       sliderInput(inputId = "TE",
@@ -63,13 +63,11 @@ ui <- fluidPage(
                           fluidRow(
                               h4("The first step is to recruit some asthma patients into the study,
                               and randomly assign each patient to one of two groups: the new medicine (orange) or a control group (green).
-                                 Patients further to the right on the below figure had more of an improvement in their asthma,"),
+                                 Patients further to the right on the below figure had more of an improvement in their asthma."),
                               h4("Q: Do you think there's any difference between patients given the new treatment compared to the control group?
-                                 Do all patients in the same group improve by the same amount?"),
+                                 Within each group, did all patients improve by the same amount?"),
     
-                              h4("Feel free to experiment with the sliders, to see the effect on the results. 
-                                 You can change how well the medicine works (in a real trial, we obviously don't know this, which is why we need to do the study!).
-                                 You can also change the number of patients (an important part of a statistician's job is to get this number right)."), 
+                              h4("Try experimenting with the sliders, to see the effect on the results."), 
                               h4(""), 
                               h4("Now let's go on to Step 2 to take a closer look..."),                             
                               
@@ -80,32 +78,50 @@ ui <- fluidPage(
                  tabPanel(h4("Step 2: Look at the Data"),
                           fluidRow(
                             h4("This figure provides allows us to compare the results for the two treatment groups. If, for example, the orange curve is shifted
-                               to the right compared to the green curve, it provides evidence that the medicine is effective compared to the control group."),
+                               to the right compared to the green curve, it shows that the medicine might be effective compared to the control group."),
                             h4(""),
-                            h4("Try moving the first slider to the right, simulating a more effective medicine. Do you notice any difference in the curves?"),
-                            h4("Now move the slider labelled 'Variability' to the right. What effect does it have on the curves? "),
+                            h4("Try moving the slider labelled 'How Well Does The Medicine Work' to the right, simulating a more effective medicine. Do you notice any difference in the curves?"),
+                            h4("Now try changing the slider labelled 'Variability'. What effect does it have on the curves? "),
                             
-                           plotOutput(outputId = "myPlot2", width = "800px"
+                           plotOutput(outputId = "myPlot2", width = "800px",
                                      ))
                           ),
-                 tabPanel(h4("Step 3: What Does It Tell Us?"), 
+                 tabPanel(h4("Step 3: Making a Decision?"), 
                           fluidRow(
-                            h4("In this figure, the coloured circle represents the effectiveness of the new medicine in the study. Try hitting the 'refresh' button to
-                               simulate re-running the study. Do we always get the same result? You'll notice we get a slightly different result each time."),
-                            h4("So what is the 'real' treatment effect (i.e. if we were to give the medicine to all asthma patients in the real world)? In practice we don't know this,
-                            but the horizontal coloured line (confidence interval) represents the likely values. So if this line is short, we're more confident in our estimate of the treatment effect,
-                            compared to a wider line."),
-                            h4("Try experimenting with the three sliders. What happens to the position and width of the confidence interval if we change each slider. 
-                               If the confidence interval turns red, it means we've made a false conclusion - which we discuss in Step 4..."),
+                           h4("To decide whether or not the medicine works, we can use a Confidence Interval (shown below as a coloured bar). If this bar is entirely to the right
+                               of the black line, we have strong enough evidence to conclude that the medicine works."),
+                            h4("Try changing the sliders to the following settings:"),
+                            h4("--How Well Does The Medicine Work? = 10"),   
+                            h4("--How Many Patients? = 50"),
+                            h4("--Variability=45"),
+                            h4("What happens to the colour of the bar? In this app, if the bar goes red it indicates a 'False Negative'. This means that even though in reality the drug works, 
+                               the study has drawn a wrong conclusion. The evidence wasn't convincing enough to conclude that the medicine works. (Unlike this app, in a real clinical trial
+                               we wouldn't know trhe truth about whether the medicine works.)"), 
+                            h4("A false negative is definitely a bad outcome! See if you can fix the problem by increasing the number of patients. One of the jobs of the statistician is to make sure we have enough patients in the study to avoid this happening. 
+                               Let's go onto Step 4 to investigate further."),
+
                           plotOutput(outputId = "myPlot3", width = "800px", height = "150px"
                                      )),
                  ),
                  tabPanel(h4("Step 4:Can We Trust the Results?"),
-                          tags$div(
-                   h3("Here are some instructions for this page.")
-                 ),
+                          fluidRow(
+                   h4("Now imagine we re-run the study several times, with the same settings. The confidence intervals are shown below."),
+                   h4("Do we get the same result every time? Another job of a statistian
+                      is to understand how much the results are likely to vary from study to study, and how often we see a false negative result."),
+                   h4("Now try changing the sliders to the following settings:"),
+                   h4("--How Well Does The Medicine Work? = 0"),   
+                   h4("--How Many Patients? = 100"),
+                   h4("--Variability=20"),
+                   h4("Here we are simulating a situation where the new medicine dosen't work. Did you notice any red confidence intervals? (If not, try hitting the Refresh button a few times,)
+                      This is an example of a False Positive, i.e. the confidence interval is entirely to the left or right of the black reference line, even though the drug doesn't work. This is a serious
+                      situation, because we don't want a medicine that doesn't work to be given to patients (due to possible side effects). Another job of the statistician is to make sure that
+                      a false positive result is highly unlikely."),
+                   h4("So we've seen that statistics plays an important role in developing new medicines, and in helping pharmaceutical companies and health authorities make the right
+                      decisions for patients."),
+                   
+                   
                           plotOutput(outputId = "myPlot4", width = "800px", height = "800px"))
-                 )
+                 ))
                )
              )
           )
@@ -178,6 +194,15 @@ server <- function(input, output, session) {
     )
   }, deleteFile = FALSE)
   
+  output$photo2 <- renderImage({
+    list(
+      src = "photos/jury.jpg",
+      contentType = "image/jpeg",
+      width = 125,
+      height = 100
+    )
+  }, deleteFile = FALSE)
+  
      output$myPlot1 <- renderPlot({
 
     df <- my_data1()
@@ -189,7 +214,7 @@ server <- function(input, output, session) {
       scale_color_manual(values=c("#1b9e77", "#d95f02")) +
       theme(plot.title = element_markdown(colour = "#636363",
                                           size = 20),
-            panel.background=element_rect(fill="#f0f0f0"),
+            panel.background=element_rect(fill="white"),
             panel.grid.major.x=element_line(colour = "#bdbdbd",
                                           linewidth = 0.5,
                                           linetype = 1),
@@ -203,9 +228,12 @@ server <- function(input, output, session) {
               size = 20),
             axis.text.y=element_blank(),
             axis.ticks.y=element_blank(),
-            axis.title=element_text(
+            axis.title.x=element_text(
               colour = "#636363",
-              size = 20)) +
+              size = 20),
+            axis.title.y=element_blank()
+            ) +
+       labs(title=("Patients Treated With <span style='color:#1b9e77'>Placebo</span> or <span style='color:#d95f02'>New Medicine</span>")) +
        geom_fontawesome(
          alias = "fa-child",
          x = df$chg,
@@ -214,7 +242,6 @@ server <- function(input, output, session) {
          alpha = 0.6,
          color = ifelse(df$trt == 0, "#1b9e77", "#d95f02")
        ) 
-       # labs(title="<b>Disease Outcome in <span style='color:#1b9e77'>Placebo</span> and <span style='color:#d95f02'>Treated</span> Patients</b>") 
        
      }) 
     
@@ -230,7 +257,7 @@ server <- function(input, output, session) {
         scale_fill_manual(values=c("#1b9e77", "#d95f02")) +
         theme(plot.title = element_markdown(colour = "#636363",
                                             size = 20),
-              panel.background=element_rect(fill="#f0f0f0"),
+              panel.background=element_rect(fill="white"),
               panel.grid.major.x=element_line(colour = "#636363",
                                             linewidth = 0.5,
                                             linetype = 1),
@@ -246,9 +273,8 @@ server <- function(input, output, session) {
               axis.ticks.y=element_blank(),
               axis.title=element_text(
                 colour = "#636363",
-                size = 20)) 
-        # labs(title="<b>Density Plot (<span style='color:#1b9e77'>Placebo</span> and <span style='color:#d95f02'>Treated</span> Groups)</b>") 
-        # 
+                size = 20)) +
+        labs(title=("Patients Treated With <span style='color:#1b9e77'>Placebo</span> or <span style='color:#d95f02'>New Medicine</span>"))
     })
 
     output$myPlot3 <- renderPlot({
@@ -257,8 +283,8 @@ server <- function(input, output, session) {
         filter(sim == 1) 
 
       ggplot(data=df) +
-        geom_errorbarh(show.legend = FALSE, aes(xmin = ci_lower, xmax = ci_upper, y = 1, color = c), size = 3, alpha = 0.7) +
-        geom_point(show.legend = FALSE, aes(x = diff_mean, y = 1, color = c, fill = c), shape = 21, size = 12, alpha = 0.7) +
+        geom_rect(show.legend = FALSE, aes(xmin = ci_lower, xmax = ci_upper, ymin = (sim - 0.2), ymax = (sim + 0.2), fill = c, color = c)) +
+        # geom_point(show.legend = FALSE, aes(x = diff_mean, y = 1, color = c, fill = c), shape = 21, size = 12, alpha = 0.7) +
         # geom_point(aes(x = as.numeric(input$TE), y = 20), shape=5, size = 8, color = "red") +
         # geom_vline(show.legend = FALSE, aes(xintercept = as.numeric(input$TE)), color = "red", linetype = "dotted", size = 0.5) +
         # geom_vline(aes(xintercept = as.numeric(input$TE)), color = "red", linetype = "dotted", size = 1) +
@@ -267,10 +293,11 @@ server <- function(input, output, session) {
                            limits=c(0,2)) +
         scale_color_manual(values = c("0" = "red", "1" = "blue")) +
         scale_fill_manual(values = c("0" = "red", "1" = "blue")) +
-        # annotate("text", label = "Confidence Interval", x = df$diff_mean, y = 8, color = "#636363", size=5) +
         theme(plot.title = element_markdown(colour = "#636363",
                                             size = 20),
-              panel.background=element_rect(fill="#f0f0f0"),
+              panel.background=element_rect(fill="white"),
+              # panel.border=element_rect(colour = "#636363",
+              #                           fill = NULL),
               panel.grid.major.x=element_line(colour = "#f0f0f0",
                                               linewidth = 0.5,
                                               linetype = 1),
@@ -293,19 +320,21 @@ server <- function(input, output, session) {
     output$myPlot4 <- renderPlot({
   
         ggplot(data=my_data2()) +
-        geom_errorbarh(show.legend = FALSE, aes(xmin = ci_lower, xmax = ci_upper, y = sim, color = c)) +
-        geom_point(show.legend = FALSE, aes(x = diff_mean, y = sim, color = c, fill = c), shape = 21, size = 2, alpha = 0.7) +
+        # geom_errorbarh(show.legend = FALSE, aes(xmin = ci_lower, xmax = ci_upper, y = sim, color = c)) +
+        geom_rect(show.legend = FALSE, aes(xmin = ci_lower, xmax = ci_upper, ymin = (sim - 0.02), ymax = (sim + 0.02), color = c)) +
+        # geom_point(show.legend = FALSE, aes(x = diff_mean, y = sim, color = c, fill = c), shape = 21, size = 2, alpha = 0.7) +
         geom_vline(show.legend = FALSE, aes(xintercept = as.numeric(input$TE)), color = "red", linetype = "dotted", size = 0.5) +
         geom_vline(show.legend = FALSE, aes(xintercept = 0), color = "#636363", size = 1) +
         scale_x_continuous("Effect of Treatment", limits=c(-50,70)
         ) +
-        scale_y_continuous("",
-                           limits=c(0,21)) +
+        scale_y_continuous(""
+                           # ,limits=c(0,21)
+                           ) +
         scale_color_manual(values = c("0" = "red", "1" = "blue")) +
         scale_fill_manual(values = c("0" = "red", "1" = "blue")) +
         theme(plot.title = element_markdown(colour = "#636363",
                                             size = 20),
-              panel.background=element_rect(fill="#f0f0f0"),
+              panel.background=element_rect(fill="white"),
               panel.grid.major.x=element_line(colour = "#f0f0f0",
                                               linewidth = 0.5,
                                               linetype = 1),
